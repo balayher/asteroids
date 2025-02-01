79319adae34c4803e033f13358c27a132ffbe540
balayher <balayher@gmail.com>
2025-01-31 19:53:15 -0800

    adding movement

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
@@ -19,10 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
         player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
