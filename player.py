04aa89447df904581f41f9c6dbf6d5aea7f39429
balayher <balayher@gmail.com>
2025-01-31 21:36:02 -0800

    adding screenwrap

--- player.py
+++ player.py
@@ -42,6 +42,14 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+        if self.position.x < 0:
+            self.position.x = SCREEN_WIDTH
+        if self.position.x > SCREEN_WIDTH:
+            self.position.x = 0
+        if self.position.y < 0:
+            self.position.y = SCREEN_HEIGHT
+        if self.position.y > SCREEN_HEIGHT:
+            self.position.y = 0
 
     def shoot(self):
         if self.shot_timer > 0:
